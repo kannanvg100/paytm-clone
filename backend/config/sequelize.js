ad6134f22807cc3dea8a97fd8b96c5b89fec8838
kannanvg100 <kannanx200@gmail.com>
2024-03-26 22:28:02 +0530

    sequelize config change

--- backend/config/sequelize.js
+++ backend/config/sequelize.js
@@ -2,6 +2,8 @@ const { Sequelize } = require('sequelize');
 
 // Initialize Sequelize
 // const sequelize = new Sequelize('postgres://postgres:admin@localhost:5432/postgres')
-const sequelize = new Sequelize(process.env.DB_URI)
+const sequelize = new Sequelize(process.env.DB_URI, {
+    dialectModule: require('pg')
+})
 
 module.exports = sequelize;
