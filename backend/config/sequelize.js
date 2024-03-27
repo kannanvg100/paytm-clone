bb7b6b716c3d07b9da85ba2bb7f3037dc9ca2ef2
Kannan VG <111639272+kannanvg100@users.noreply.github.com>
2024-03-27 12:24:04 +0530

    Update sequelize.js

--- backend/config/sequelize.js
+++ backend/config/sequelize.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 
 // Initialize Sequelize
 // const sequelize = new Sequelize('postgres://postgres:admin@localhost:5432/postgres')
-const sequelize = new Sequelize(process.env.DB_URI, {
+const sequelize = new Sequelize(process.env.POSTGRES_URL, {
     dialectModule: require('pg')
 })
 
